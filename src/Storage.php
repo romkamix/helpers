95b6a1a8e719da22132c0e2445a95bf930dd28b5
romkamix <romkamix@gmail.com>
2022-12-07 12:14:42 +0300

    20221207_1214

--- src/Storage.php
+++ src/Storage.php
@@ -2,6 +2,7 @@
 
 namespace Rmx;
 
+use FilesystemIterator;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 
@@ -10,20 +11,20 @@ class Storage
     public static function removeEmptyDirectories(String $path): void
     {
         foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::CHILD_FIRST) as $file) {
-            $path = $file->getPathname();
+            $pathname = $file->getPathname();
 
             if (
                 $file->isDir()
-                && mb_substr($path, -1) !== '.'
-                && self::isDirEmpty($path)
+                && mb_substr($pathname, -1) !== '.'
+                && self::isDirEmpty($pathname)
             ) {
-                rmdir($path);
+                rmdir($pathname);
             }
         }
     }
 
     public static function isDirEmpty(String $dir): bool
     {
-        return !(new \FilesystemIterator($dir))->valid();
+        return !(new FilesystemIterator($dir))->valid();
     }
 }
